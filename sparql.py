66dac9b0e2b6e1e21add27f765e106dae944183a
Mengshi Ma <mengshim@mail.upb.de>
2021-10-27 14:49:29 +0200

    tried out another query

--- sparql.py
+++ sparql.py
@@ -8,12 +8,16 @@ ssl._create_default_https_context = ssl._create_unverified_context
 
 sparql = SPARQLWrapper("http://dbpedia.org/sparql")
 sparql.setQuery("""
-    PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
-    SELECT ?label
-    WHERE { <http://dbpedia.org/resource/Asturias> rdfs:label ?label }
+    PREFIX dbprop: <http://dbpedia.org/ontology/>
+PREFIX db: <http://dbpedia.org/resource/>
+SELECT ?who, ?WORK, ?genre WHERE {
+  db:Tokyo_Mew_Mew dbprop:author ?who . 
+  ?WORK  dbprop:author ?who .
+OPTIONAL { ?WORK dbprop:genre ?genre } .
+}
 """)
 sparql.setReturnFormat(JSON)
 results = sparql.query().convert()
 
 for result in results["results"]["bindings"]:
-    print(result["label"]["value"])
+    print(result["who"], result["WORK"], result.get("genre", None))
