19a92dbdc4ab136a0425081747160edd5116aa9e
Yulia Dziuba <dzyyulia@gmail.com>
2021-10-31 13:56:03 +0100

    Update qa.py
    Komments, procedure ask_DBPedia with 2 parameters (Subject, property) sends request depending from subj and property.

--- TEAM2/qa.py
+++ TEAM2/qa.py
@@ -3,20 +3,14 @@ from pycorenlp import StanfordCoreNLP
 import ssl
 import json
 from ctypes.test.test_pickling import name
+
+# fix Python SSL CERTIFICATE_VERIFY_FAILED during access to DBPedia
 ssl._create_default_https_context = ssl._create_unverified_context
 
 text = (
-  'What is the birthsday of Barack Obama?')
-sparql_query='''
-SELECT *
-WHERE
-{
-  ?person  rdfs:label  ?label;
-               dbp:office ?office.
- FILTER(?label="Obama Barack"@en || ?label="Barack Obama"@en)
-}'''
+  'Who is Barack Obama?')
 
-
+# parse NLP JSON in order to find a subject(nsubj) and all compounds of it
 def parse_nlp(text):
     output = json.loads(text)
     search = output['sentences'][0]['basicDependencies']
@@ -29,7 +23,7 @@ def parse_nlp(text):
         if item['dep']=='compound' and item['governorGloss']==nsubj:
             subj=item['dependentGloss']+' '+subj
     return subj    
-
+# sends request to NLP for the parameter "question", returns json with analysis and dependences in the question    
 def ask_coreNLP(question):
     nlp = StanfordCoreNLP('http://localhost:9000')
     output = nlp.annotate(text, 
@@ -38,36 +32,36 @@ def ask_coreNLP(question):
     'outputFormat': 'json'
     })
     return output
-
-def ask_DBpedia(name):
+# sends request to DBPedia with a query for subject and property in the parameters, returns json with information about subject and his property
+def ask_DBpedia(subject, property):
     sparql_query='''
         SELECT *
         WHERE
         {
-          ?person  rdfs:label  ?label;
-                       dbp:office ?office.
-         FILTER(?label="'''+name+'''"@en)
+          ?person   rdfs:label  ?label; 
+           '''+'''dbp:'''+property+'''?'''+property+'''.'''+'''
+                    
+         FILTER(?label="'''+subject+'''"@en)
         }'''
-    answers = []
     sparql = SPARQLWrapper("https://dbpedia.org/sparql")
     sparql.setQuery(sparql_query)
     sparql.setReturnFormat(JSON)
     queryresult = sparql.query().convert()
     return queryresult
-
+# parse the json from DBPedia in order to find value of property
 def parse_answer(db_answer):
-    #output = json.loads(db_answer)
     answer = db_answer['results']['bindings'][0]['office']['value']
     return answer  
  
 
 nlp = ask_coreNLP(text)
-print(nlp)
-#name = parse_nlp(nlp)
-#print(name)
-#answer = ask_DBpedia(name)
-#db_answer = parse_answer(answer)
+#print(nlp)
+name = parse_nlp(nlp)
+property = 'office'
+print(name)
+answer = ask_DBpedia(name, property)
 #print(answer)
-#print(db_answer)
+db_answer = parse_answer(answer)
+print(db_answer)
 
 
