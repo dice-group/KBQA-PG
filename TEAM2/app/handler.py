ecb03db1455559d63587ac736c7377401c1fb1c8
Tejas <tejasdev1995@gmail.com>
2021-11-01 11:26:29 -0700

    Add files via upload
    added changes to parse multiple answers and URI type answer.

--- TEAM2/app/handler.py
+++ TEAM2/app/handler.py
@@ -1,7 +1,9 @@
 from SPARQLWrapper import SPARQLWrapper, JSON
+#from pycorenlp import StanfordCoreNLP
 from app.query import generate_queries
 import spacy
 from spacy.symbols import nsubj, VERB
+from rdflib import URIRef #modified for parsing answer of type - uri
 nlp = spacy.load("en_core_web_sm")
 
 #converts geberated resouce name into db-pedia equivalent key_word
@@ -40,8 +42,6 @@ def parse_nlp(text):
 
     return recs_ent, prop_ent
 
-
-#ask questions to dbpedia using sparql
 def ask_DBpedia(subject, property):
     sparql_query='''
         SELECT *
@@ -58,9 +58,6 @@ def ask_DBpedia(subject, property):
     queryresult = sparql.query().convert()
     return queryresult
 
-
-
-#parse the response of dbpedia response
 def parse_answer(db_answer, prop_ans):
     answer = db_answer['results']['bindings'][0][prop_ans]['value']
     return answer
@@ -73,6 +70,17 @@ def parse_answer(db_answer, prop_ans):
 
 # handle incoming questions and format the answers
 def process_question(question):
+
+	# ------------------------------------------------
+	# TODO add here the logic to process the question
+	# and return the result from dbpedia
+	# ------------------------------------------------
+
+	# core_nlp_result = ask_core_nlp(question)
+	# ent, tok = parse_core_nlp_result(core_nlp_result)
+	# print('Entities:', ent)
+
+
 	recs_ent, prop_ent = parse_nlp(question)
 	name = resource_generator(recs_ent)
 	prop = property_generator(prop_ent)
@@ -85,8 +93,74 @@ def process_question(question):
 
 	return answers
 
+	# -------------------------- BEGIN EXAMPLE ---------------------------------------
+	# example in order to check, whether the system works
+
+	example_question = 'Who is the chancellor of Germany?'
+	example_query = '''
+		SELECT ?name 
+		WHERE {
+			dbr:Germany dbp:leaderName ?name .
+			?name dbp:title dbr:Chancellor_of_Germany .
+		}
+	'''
+
+	if question == example_question:
+		dbpedia_result = ask_dbpedia(example_query)
+	else:
+		# case for unsupported question
+		dbpedia_result = ask_dbpedia('SELECT * WHERE { dbp:no dbp:answer dbp:expected }')
+
+	return dbpedia_result
+
+	# -------------------------- END EXAMPLE ---------------------------------------------
+
+# 
 
 
 
 
+
+# def parse_dbpedia_results(dbpedia_results):
+# 	answers = list()
+
+# 	for dbpedia_result in dbpedia_results:
+
+# 		variables = dbpedia_result['head']['vars']
+
+# 		for result in dbpedia_result['results']['bindings']:
+# 			for variable in variables:
+# 				answer = result.get(variable)['value']
+# 				answer = answer.split('/')[-1]
+
+# 				answers.append(answer)
+
+# 	return answers
+
+
+# sends request to DBPedia with a query for subject and property in the parameters, returns json with information about subject and his property
+
+
+def parse_answer(db_answer, prop_ans):
+    answers =[]
+    answer = ''
+    # print((db_answer['results']['bindings']))
+    for i in range(len(db_answer['results']['bindings'])):
+        if db_answer['results']['bindings'][i][prop_ans]['type'] == 'uri':
+            resource = URIRef(db_answer['results']['bindings'][i][prop_ans]['value'])
+            string_value = resource.split('/')[-1].replace('_',' ')
+            answers.append(string_value)
+        else:
+            answers.append(db_answer['results']['bindings'][i][prop_ans]['value'])
+    # answer = db_answer['results']['bindings'][0][prop_ans]['value']
+    answer = ",".join(str(x) for x in answers)
+    return answer  
  
+# recs_ent, prop_ent = parse_nlp(text)
+# name = resource_generator(recs_ent)
+# prop = property_generator(prop_ent)
+
+
+# answer = ask_DBpedia(name, prop)
+# db_answer = parse_answer(answer, prop)
+# print(db_answer)
