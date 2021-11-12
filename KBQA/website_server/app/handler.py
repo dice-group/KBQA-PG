3130217c7bb2c5f8207ba04f36749fdb2461497d
Michael Skowronek <michael.skowronek.91@gmail.com>
2021-11-12 23:04:45 +0100

    Client<->Website working, Website<->App left.

--- KBQA/website_server/app/handler.py
+++ KBQA/website_server/app/handler.py
@@ -1,135 +1,89 @@
-import spacy
-
-from SPARQLWrapper import SPARQLWrapper, JSON
-from app.query import generate_queries
-from spacy.symbols import nsubj, VERB
 from rdflib import URIRef
 
 
-nlp = spacy.load("en_core_web_sm")
-
-
-def resource_generator(resource_name):
-    '''
-    Convert the generated resource name into DBpedia equivalent key_word
-    '''
-
-    words = [word.lower() for word in resource_name.split()]
-    key = ' '.join([word.capitalize() for word in words ])
-
-    return key
-
-
-def property_generator(property_name):
-    '''
-    Convert the generated property name into DBpedia equivalent property
-    '''
-
-    words = [word.lower() for word in property_name.split()]
-    prop_names = []
-
-    for word in words:
-        if word != words[0]:
-            word = word.capitalize()
-        prop_names.append(word)
-    
-    prop = ''.join([word for word in prop_names ])
-
-    return prop
-
-
-def parse_nlp(text):
-    '''
-    Parse the question as NLP string and extract the name entity and property
-    '''
-
-    doc = nlp(text)
-    recs_ent = doc.ents[0].text
-
-    prop_ents = []
-
-    for chunk in doc.noun_chunks:
-        if str(chunk) == recs_ent:
-            continue
-        prop_ents.append( " ".join(str(word)  for word in chunk if not word.is_stop))
-
-    prop_ents = [prop for prop in prop_ents if prop]
-   
-    prop_ent = prop_ents[0]
-
-    return recs_ent, prop_ent
-
-
-def ask_DBpedia(subject, predicate):
-    '''
-    Send a simple SPARQL query to DBpedia containing the parameters as subject and predicate
-    '''
-
-    subject = subject.replace(' ', '_')
-    sparql_query = 'SELECT ?o WHERE {{ dbr:{} dbp:{} ?o }}'.format(subject, predicate)
-
-    sparql = SPARQLWrapper("https://dbpedia.org/sparql")
-    sparql.setQuery(sparql_query)
-    sparql.setReturnFormat(JSON)
-    queryresult = sparql.query().convert()
-
-    return queryresult
-
-
-def process_question(question):
-    '''
+def process_question(question, app):
+    """
     Handle an incoming question and format the answers
-    '''
+    """
 
-    recs_ent, prop_ent = parse_nlp(question)
-    name = resource_generator(recs_ent)
-    prop = property_generator(prop_ent)
+    # POST to nginx to get answer_QALD
 
-    dbpedia_ans = ask_DBpedia(name, prop)
+    # Example answers
+    if app == 'A':
+        answer_QALD = {
+            "questions": [{
+                "id": "1",
+                "question": [{
+                    "language": "en",
+                    "string": "Which German cities have more than 250000 inhabitants?"
+                }],
+                "query": {
+                    "sparql": "SELECT DISTINCT ?uri WHERE { { ?uri <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <http://dbpedia.org/ontology/City> . } UNION { ?uri <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <http://dbpedia.org/ontology/Town> . }  ?uri <http://dbpedia.org/ontology/country> <http://dbpedia.org/resource/Germany> .  ?uri <http://dbpedia.org/ontology/populationTotal> ?population .  FILTER ( ?population > 250000 ) } "
+                },
+                "answers": [{
+                    "head": {
+                        "vars": [
+                            "uri"
+                        ]
+                    },
+                    "results": {
+                        "bindings": [{
+                            "uri": {
+                                "type": "uri",
+                                "value": "http://dbpedia.org/resource/A"
+                            }
+                        }]
+                    }
+                }]
+            }]
+        }
+    if app == 'B':
+        answer_QALD = {
+            "questions": [{
+                "id": "1",
+                "question": [{
+                    "language": "en",
+                    "string": "Which German cities have more than 250000 inhabitants?"
+                }],
+                "query": {
+                    "sparql": "SELECT DISTINCT ?uri WHERE { { ?uri <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <http://dbpedia.org/ontology/City> . } UNION { ?uri <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <http://dbpedia.org/ontology/Town> . }  ?uri <http://dbpedia.org/ontology/country> <http://dbpedia.org/resource/Germany> .  ?uri <http://dbpedia.org/ontology/populationTotal> ?population .  FILTER ( ?population > 250000 ) } "
+                },
+                "answers": [{
+                    "head": {
+                        "vars": [
+                            "uri"
+                        ]
+                    },
+                    "results": {
+                        "bindings": [{
+                            "uri": {
+                                "type": "uri",
+                                "value": "http://dbpedia.org/resource/B"
+                            }
+                        }]
+                    }
+                }]
+            }]
+        }
 
-    answers = parse_answer(dbpedia_ans, 'o')
+    answer_parsed = parse_answer(answer_QALD['questions'][0]['answers'][0], 'uri')
 
-    return answers
-
-    # -------------------------- BEGIN EXAMPLE ---------------------------------------
-    # example in order to check, whether the system works
-
-    # example_question = 'Who is the chancellor of Germany?'
-    # example_query = '''
-    # 	SELECT ?name 
-    # 	WHERE {
-    # 		dbr:Germany dbp:leaderName ?name .
-    # 		?name dbp:title dbr:Chancellor_of_Germany .
-    # 	}
-    # '''
-
-    # if question == example_question:
-    # 	dbpedia_result = ask_dbpedia(example_query)
-    # else:
-    # 	# case for unsupported question
-    # 	dbpedia_result = ask_dbpedia('SELECT * WHERE { dbp:no dbp:answer dbp:expected }')
-
-    # return dbpedia_result
-
-    # -------------------------- END EXAMPLE ---------------------------------------------
+    return answer_parsed
 
 
 def parse_answer(db_answer, prop_ans):
-    '''
+    """
     Parse the result from DBpedia and extract the answer for the searched property
-    '''
+    """
 
     answers = []
-    answer = ''
-    
     for i in range(len(db_answer['results']['bindings'])):
         if db_answer['results']['bindings'][i][prop_ans]['type'] == 'uri':
             resource = URIRef(db_answer['results']['bindings'][i][prop_ans]['value'])
-            string_value = resource.split('/')[-1].replace('_',' ')
+            string_value = resource.n3()
             answers.append(string_value)
         else:
             answers.append(db_answer['results']['bindings'][i][prop_ans]['value'])
-    
     answer = ", ".join(str(x) for x in answers)
 
-    return answer  
+    return answer
--- KBQA/website_server/app/routes.py
+++ KBQA/website_server/app/routes.py
@@ -2,25 +2,24 @@ from app import application
 from flask import render_template, request, redirect, url_for
 from app.handler import process_question
 
-@application.route('/')
-# @application.route('/index')
-# def index():
-#     return render_template('index.html')
 
-@application.route('/', methods=['GET', 'POST'])
-def question():
-
-    if request.method == 'POST':
-        question = request.form['question']
-        print('Question', question)
-
-        answers = process_question(question)
-        print('Answers', answers)
-
-        return render_template('question.html', question=question, answers=answers)
-    
+@application.route('/', methods=['GET'])
+def index():
     return render_template('question.html')
+
+
+@application.route('/WEB', methods=['POST'])
+def question():
+    asked_question = request.form['query']
+    chosen_app = request.form['App']
+    print('Question', asked_question)
+
+    answers = process_question(asked_question, chosen_app)
+    print('Answers', answers)
+
+    return render_template('question.html', query=asked_question, answers=answers)
+
 
 @application.errorhandler(500)
 def internal_error(error):
-    return render_template('500.html'),500
+    return render_template('500.html'), 500
