1a3e1f0e2d20606528297026913e217c9fe43d7b
Mengshi Ma <makevin23@gmail.com>
2022-01-23 19:27:12 +0100

    fixed ssl certificate

--- KBQA/kbqa/webservice/appA/app_a_wsgi.py
+++ KBQA/kbqa/webservice/appA/app_a_wsgi.py
@@ -1,6 +1,9 @@
 """Start the service for approach A, when this module is called."""
+import ssl
 from app.api import application
 
+ssl._create_default_https_context = ssl._create_unverified_context
 
 if __name__ == "__main__":
+    
     application.run(port=24801)
