0ff9c67a198b447971172534ed56cc6108774339
Mengshi Ma <makevin23@gmail.com>
2022-02-03 13:25:34 +0100

    extracted load model

--- KBQA/kbqa/webservice/appA/app/nspm/interpreter.py
+++ KBQA/kbqa/webservice/appA/app/nspm/interpreter.py
@@ -123,7 +123,7 @@ def plot_attention(attention, sentence, predicted_sentence,ou_dir):
   fig.savefig('{}/graph.png'.format(ou_dir))
 
 
-def translate(sentence,ou_dir):
+def translate(sentence):
   result, sentence, attention_plot = evaluate(sentence)
 
   print('Input: %s' % (sentence))
@@ -155,37 +155,40 @@ def locate_model(url):
         raise Exception(output['message'])
 
 def process_question(question):
-    global max_length_targ,max_length_inp,encoder,decoder,units,w2v_model,w2v_model_spql
 
-    inputstr = question
-    inputs = inputstr
-    input = 'data/new_qald8'
-    output = 'data/new_qald8'
-    airml = None
+    inputs = question
+
+    finaltrans = "input qurey : \n"
+    finaltrans += inputs
+    finaltrans += "\n \n \n output qurey : \n"
+    finaltranso = translate(inputs)
+    finaltrans += finaltranso
+    finaltrans += '\n \n \n output query decoded : \n'
+    finaltranso = decode(finaltranso)
+    finaltranso = fix_URI(finaltranso)
+    # print('Decoded translation: {}'.format(finaltranso))
+    finaltrans += finaltranso
+    # outputfile = open((input_dir+'/output_query.txt'),'w',encoding="utf8")
+    # outputfile.writelines([finaltrans])
+    # outputfile.close()
+    print(finaltrans)
+    return(finaltranso.split('<end>')[0])
+
+def load_model():
+    global max_length_targ,max_length_inp,encoder,decoder,units,w2v_model,w2v_model_spql, checkpoint
 
     model_dir = 'data/new_qald8'
     input_dir = 'data/new_qald8'
-    if input is not None:
-        model_dir = input
-        input_dir = input
-    elif airml is not None:
-        airml_url = airml
-        model_dir = locate_model(airml_url)
-        input_dir = model_dir
-    else:
-        print('--input or --airml argument should be provided to load the model.')
-        sys.exit()
 
     model_dir += '/training_checkpoints'
     pic_dir = input_dir + '/pickle_objects'
 
-    print(model_dir)
+    # print(model_dir)
     embedding_dim = 300
     units = 1024
-    
 
+    global w2v_model, w2v_model_spql
     w2v_model = FastText.load(pic_dir+'/model_nl.model')
-
     w2v_model_spql = FastText.load(pic_dir+'/model_spql.model')
 
     # with open(pic_dir+'/BATCH_SIZE.pickle', 'rb') as f:
@@ -201,14 +204,13 @@ def process_question(question):
 
     # Calculate max_length of the target tensors'
     max_length_targ = input_tensor.shape[1]
-    print('max_length_targ', max_length_targ)
+    # print('max_length_targ', max_length_targ)
     max_length_inp = target_tensor.shape[1]
 
     BATCH_SIZE = 32
 
     vocab_inp_size = len(embedding_matrix)
     vocab_tar_size = len(embedding_matrix_spql)
-
 
     encoder = Encoder(vocab_inp_size, embedding_dim, units, BATCH_SIZE,embedding_matrix)
     decoder = Decoder(vocab_tar_size, embedding_dim, units, BATCH_SIZE,embedding_matrix_spql)
@@ -218,22 +220,6 @@ def process_question(question):
                                  encoder=encoder,
                                  decoder=decoder)
 
-
     checkpoint.restore(tf.train.latest_checkpoint(model_dir)).expect_partial()
 
-
-    finaltrans = "input qurey : \n"
-    finaltrans += inputs
-    finaltrans += "\n \n \n output qurey : \n"
-    print(inputs,input_dir)
-    finaltranso = translate(inputs,input_dir)
-    finaltrans += finaltranso
-    finaltrans += '\n \n \n output query decoded : \n'
-    finaltranso = decode(finaltranso)
-    finaltranso = fix_URI(finaltranso)
-    # print('Decoded translation: {}'.format(finaltranso))
-    finaltrans += finaltranso
-    # outputfile = open((input_dir+'/output_query.txt'),'w',encoding="utf8")
-    # outputfile.writelines([finaltrans])
-    # outputfile.close()
-    return(finaltranso.split('<end>')[0])
+load_model()
