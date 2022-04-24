fb11f28990a50ba6b06723495bf74cd419cb16bc
sapkotaruz11 <sapkotarupesh15@gmail.com>
2022-04-24 12:07:53 +0200

    tripleBERT model

--- KBQA/appB/transformer_architectures/bert_spbert_spbert_base/bert_triple_encoder.py
+++ KBQA/appB/transformer_architectures/bert_spbert_spbert_base/bert_triple_encoder.py
@@ -3,9 +3,21 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.nn.TransformerEncoder as TransformerEncoder
+import torch.nn.TransformerEncoderLayer as EncoderLayer 
+
 
 class ConexEmbedding(nn.Module):
-        pass
+        def __init__(self, max_len, hidden_size):
+            super(ConexEmbedding, self).__init__()
+            self.positional_embedding = nn.Embedding(max_len, hidden_size)
+            positions = torch.arrange(0, max_len)
+            self.register_buffer('positions', positions)
+
+
+        def forward(self, sequence):
+            batch_size, seq_len = sequence.size()
+            positions = self.positions[:seq_len].unsqueeze(0).repeat(batch_size, 1)
+            return self.positional_embedding(positions)
 
 
 class SegmentEmbedding(nn.Module):
@@ -16,6 +28,28 @@ class SegmentEmbedding(nn.Module):
 
         def forward(self, segments):
             return self.segment_embedding(segments)   #batch_size, seq_len, hidden_size)
+
+
+
+def build_model(layers_count, hidden_size, heads_count, d_ff, dropout_prob, max_len, vocabulary_size):
+    conex_embedding = ConexEmbedding(num_embeddings = vocabulary_size, embedding_dim = hidden_size)
+    segment_embedding =SegmentEmbedding(hidden_size = hidden_size)
+
+
+    encoder_layer = EncoderLayer(d_model = hidden_size, nhead = heads_count)
+    encoder = TransformerEncoder(
+     encoder_layer, num_layers = layers_count
+
+     )
+
+    TripleBERT = TripleBERT(
+        encoder = encoder,
+        conex_embedding = conex_embedding,
+        segment_embedding = segment_embedding,
+        hidden_size = hidden_size,
+        vocabulary_size = vocabulary_size
+    )
+
 
 
 class TripleBERT(nn.Module):
@@ -32,9 +66,10 @@ class TripleBERT(nn.Module):
 
         def forward(self, inputs):
             sequence, segment = inputs
-            triple_embedded = self.conex_embedding(sequbece)
+            triple_embedded = self.conex_embedding(sequence)
             segment_embedded = self.segment_embedding(segment)
             embedded_sources = triple_embedded + segment_embedded
+            #embedded_sources =  segment_embedded
 
             mask = pad_masking(sequence)
             encoded_sources = self.encoder(embedded_sources, mask)
