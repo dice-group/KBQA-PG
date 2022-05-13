80c948553f9b4de9391f04709b5f88139e7d00ae
Sven Meyer <s.v.meyer@online.de>
2022-05-13 11:17:35 +0200

    Change paths to avoid empty directories

--- KBQA/kbqa/webservice/appB/app/arguments/args_postprocessing.py
+++ KBQA/kbqa/webservice/appB/app/arguments/args_postprocessing.py
@@ -4,7 +4,7 @@ from types import SimpleNamespace
 POSTPROCESSING = SimpleNamespace(
     **{
         # path tp predict_file
-        "predict_dir": "app/output/",
+        "predict_dir": "app/data",
         # predict file
         "predict_file": "predict_0.output",
     }
