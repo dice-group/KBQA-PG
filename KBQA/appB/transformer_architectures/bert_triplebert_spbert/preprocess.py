8d32b38922634b056a4f31266bec7acfa3a892d6
sapkotaruz11 <sapkotarupesh15@gmail.com>
2022-06-19 18:25:54 +0200

    minor tripleBERT evaluation pipeline update

--- KBQA/appB/transformer_architectures/bert_triplebert_spbert/preprocess.py
+++
@@ -1,7 +0,0 @@
-import os
-from KBQA.appB.preprocessing.labeling_preprocessor.labeling_preprocessor import preprocess_qtq_file
-
-
-cwd = os.getcwd()
-
-preprocess_qtq_file('data/qald-8-test.json', cwd+'/data/test_data')
--- KBQA/appB/transformer_architectures/bert_triplebert_spbert/tripleBERT/preprocess.py
+++
@@ -1,8 +0,0 @@
-import os
-from KBQA.appB.preprocessing.basic_preprocessor.basic_preprocessor import preprocess_qtq_file, preprocess_triples_file
-
-
-cwd = os.getcwd()
-
-preprocess_qtq_file('data_files/qtq-updated-qald-8-9-merged-train-multilingual.json', cwd+'/data_files')
-preprocess_triples_file('data_files/qtq-updated-qald-8-9-merged-train-multilingual.triple')
